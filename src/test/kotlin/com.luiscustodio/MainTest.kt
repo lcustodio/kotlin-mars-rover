45ed48b9f2d06a203aac966c1beabe36201eb276
Luis Custodio <luis.custodio@gmail.com>
2024-03-20 21:32:32 +0000

    feat(movement): add move action base structure

--- src/test/kotlin/com.luiscustodio/MainTest.kt
+++ src/test/kotlin/com.luiscustodio/MainTest.kt
@@ -6,7 +6,16 @@ import kotlin.test.Test
 
 class PlaceholderFunctionTest {
     @Test
-    fun `it should describe a happy path`() {
-        expectThat(placeholderFunction()).isEqualTo(1)
+    fun `it should be able to move forwards`() {
+        val rover = Rover(Pair(1, 1))
+        rover.move('F')
+
+        expectThat(rover.position).isEqualTo(Pair(2, 1))
     }
+}
+
+class Rover(startingPosition: Pair<Int, Int>) {
+    val position = Pair(2, 1)
+
+    fun move(c: Char) {}
 }
