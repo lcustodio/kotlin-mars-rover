9cd711c152254d85cbf32eab4b064ed978666024
Luis Custodio <luis.custodio@gmail.com>
2024-03-20 21:36:52 +0000

    feat(movement): implements moving forward

--- src/test/kotlin/com.luiscustodio/MainTest.kt
+++ src/test/kotlin/com.luiscustodio/MainTest.kt
@@ -12,10 +12,25 @@ class PlaceholderFunctionTest {
 
         expectThat(rover.position).isEqualTo(Pair(2, 1))
     }
+
+    @Test
+    fun `it should be able to move forwards twice in a row`() {
+        val rover = Rover(Pair(1, 1))
+        rover.move('F')
+        rover.move('F')
+
+        expectThat(rover.position).isEqualTo(Pair(3, 1))
+    }
 }
 
 class Rover(startingPosition: Pair<Int, Int>) {
-    val position = Pair(2, 1)
+    var position = startingPosition
 
-    fun move(c: Char) {}
+    fun move(c: Char) {
+        position =
+            when (c) {
+                'F' -> Pair(position.first + 1, position.second)
+                else -> Pair(position.first, position.second)
+            }
+    }
 }
