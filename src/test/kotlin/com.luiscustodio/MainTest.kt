b517070065191d0d1f2d5c60197f9ec5b8040767
Luis Custodio <luis.custodio@gmail.com>
2024-03-20 21:53:17 +0000

    feat(turning): add concept of turning and refactor move

--- src/test/kotlin/com.luiscustodio/MainTest.kt
+++ src/test/kotlin/com.luiscustodio/MainTest.kt
@@ -8,7 +8,7 @@ class PlaceholderFunctionTest {
     @Test
     fun `it should be able to move forwards`() {
         val rover = Rover(Pair(1, 1))
-        rover.move('F')
+        rover.moveForward()
 
         expectThat(rover.position).isEqualTo(Pair(2, 1))
     }
@@ -16,21 +16,57 @@ class PlaceholderFunctionTest {
     @Test
     fun `it should be able to move forwards twice in a row`() {
         val rover = Rover(Pair(1, 1))
-        rover.move('F')
-        rover.move('F')
+        rover.moveForward()
+        rover.moveForward()
 
         expectThat(rover.position).isEqualTo(Pair(3, 1))
+    }
+
+    @Test
+    fun `it should be able to move left`() {
+        val rover = Rover(Pair(1, 1))
+        rover.turn('L')
+        rover.moveForward()
+
+        expectThat(rover.position).isEqualTo(Pair(1, 0))
     }
 }
 
 class Rover(startingPosition: Pair<Int, Int>) {
     var position = startingPosition
+    private var direction: Direction = Direction.NORTH
 
-    fun move(c: Char) {
+    fun moveForward() {
         position =
-            when (c) {
-                'F' -> Pair(position.first + 1, position.second)
-                else -> Pair(position.first, position.second)
+            when (direction) {
+                Direction.NORTH -> Pair(position.first + 1, position.second)
+                Direction.EAST -> TODO()
+                Direction.SOUTH -> TODO()
+                Direction.WEST -> Pair(position.first, position.second - 1)
             }
+    }
+
+    fun turn(c: Char) {
+        direction =
+            when (c) {
+                'L' -> Direction.previous(direction)
+                else -> direction
+            }
+    }
+}
+
+enum class Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+    ;
+
+    companion object {
+        fun previous(currentEnum: Direction): Direction {
+            val values = entries.toTypedArray()
+            val previousIndex = (currentEnum.ordinal - 1 + values.size) % values.size
+            return values[previousIndex]
+        }
     }
 }
